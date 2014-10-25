f759394e16df9c456ed1a833afc73129402ed756
nikhilbangar <nikhil.s.bangar@gmail.com>
2014-10-25 23:15:47 +0530

    three sub-module files added

--- Angular_Jersy_Mongo_Tomcat_App/src/main/webapp/app/js/app.js
+++ Angular_Jersy_Mongo_Tomcat_App/src/main/webapp/app/js/app.js
@@ -17,6 +17,18 @@ qaApp.config(['$stateProvider','$urlRouterProvider',
         state('modules', {
         	url: '/modules',
             templateUrl: 'views/modules.html'
+        }).
+        state('modules.module1', {
+        	url: '/module1',
+            templateUrl: 'views/module1.html'
+        }).
+        state('modules.module2', {
+        	url: '/module2',
+            templateUrl: 'views/module2.html'
+        }).
+        state('modules.module3', {
+        	url: '/module3',
+            templateUrl: 'views/module3.html'
         });
 	
 }]);
