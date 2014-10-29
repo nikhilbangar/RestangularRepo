ab9a92d9f07ffd0742e90756f6b4a8305b837852
sanjay13prusti <sanjay13prusti@users.noreply.github.com>
2014-10-29 11:09:02 +0530

    Update app.js
    Updating after adding routes for login, signup and home

--- Angular_Jersy_Mongo_Tomcat_App/src/main/webapp/js/app.js
+++ Angular_Jersy_Mongo_Tomcat_App/src/main/webapp/js/app.js
@@ -1,34 +1,32 @@
 "use strict"
-var qaApp = angular.module('qaApp',['ui.router','ngResource','wijspread','ui.bootstrap','dialogs','qaControllers','qaServices']);
-qaApp.config(['$stateProvider','$urlRouterProvider',
-    function ($stateProvider,$urlRouterProvider) {
-    
-	$urlRouterProvider.otherwise('/login');
-	
-	$stateProvider.
-        state('login', {
-        	url: '/login',
-            templateUrl: 'views/login.html'
-        }).
-        state('signup', {
-        	url: '/signup',
-            templateUrl: 'views/signup.html'
-        }).
-        state('modules', {
-        	url: '/modules',
-            templateUrl: 'views/modules.html'
-        }).
-        state('modules.module1', {
-        	url: '/module1',
-            templateUrl: 'views/module1.html'
-        }).
-        state('modules.module2', {
-        	url: '/module2',
-            templateUrl: 'views/module2.html'
-        }).
-        state('modules.module3', {
-        	url: '/module3',
-            templateUrl: 'views/module3.html'
-        });
-	
-}]);
+var qaApp = angular.module('qaApp', [ 'ui.router', 'ngResource', 'wijspread',
+		'ui.bootstrap', 'dialogs', 'qaControllers', 'qaServices' ]);
+qaApp.config([ '$stateProvider', '$urlRouterProvider',
+		function($stateProvider, $urlRouterProvider) {
+
+			$stateProvider.state('login', {
+				url : '/login',
+				templateUrl : 'views/login.html',
+				controller : 'LoginController'
+			}).state('signup', {
+				url : '/signup',
+				templateUrl : 'views/signup.html',
+				controller : 'SignupController'
+			}).state('home', {
+				url : '/home',
+				templateUrl : 'views/home.html',
+				controller : 'HomeController'
+
+			}).state('modules.module1', {
+				url : '/module1',
+				templateUrl : 'views/module1.html'
+			}).state('modules.module2', {
+				url : '/module2',
+				templateUrl : 'views/module2.html'
+			}).state('modules.module3', {
+				url : '/module3',
+				templateUrl : 'views/module3.html'
+			});
+
+			$urlRouterProvider.otherwise('/login');
+		} ]);
