5ce4bd41f462953a728a617346f4075472b8af56
sanjay13prusti <sanjay13prusti@users.noreply.github.com>
2014-10-29 11:00:41 +0530

    Update User.java
    Updating after adding description provided

--- Angular_Jersy_Mongo_Tomcat_App/src/main/java/org/atmecs/model/User.java
+++ Angular_Jersy_Mongo_Tomcat_App/src/main/java/org/atmecs/model/User.java
@@ -1,5 +1,18 @@
 package org.atmecs.model;
 
-public class User {
+import java.io.Serializable;
+
+/***
+ * 
+ * @author Nikhil
+ * @date 10-oct-2014
+ * @description : this class represents user information
+ */
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
