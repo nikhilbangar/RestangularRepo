d4adf539731888fa1806869c76496b4033f3d5b3
sanjay13prusti <sanjay13prusti@users.noreply.github.com>
2014-10-29 11:06:07 +0530

    Update services.js
    updating after adding login and register functionality

--- Angular_Jersy_Mongo_Tomcat_App/src/main/webapp/js/services.js
+++ Angular_Jersy_Mongo_Tomcat_App/src/main/webapp/js/services.js
@@ -1,21 +1,49 @@
 'use strict'
-/*services*/
+/* services */
+
+var userInfo;
 var qaServices = angular.module('qaServices', []);
 
-qaServices.factory('QAScriptFactory' , function ($http) {
-	var factory={};
-	factory.getSheetData=function(){
-		return $http.get("/ngdemo/rest/qascript");
+qaServices.factory('QAScriptFactory', function($http, $q, $window) {
+	var factory = {};
+	factory.getSheetData = function() {
+		return $http.get("/Angular_Jersy_Mongo_Tomcat_App/rest/qascript/get");
 	};
-	factory.postSheetData=function(scriptData){
+	factory.postSheetData = function(scriptData) {
 		return $http.post("/ngdemo/rest/qascript", scriptData);
 	};
+
+	return {
+		register : function(user, success, error) {
+
+			$http.post('/Angular_Jersy_Mongo_Tomcat_App/rest/qascript/post',
+					user).success(function(res) {
+				success();
+			}).error(error);
+		},
+		login : function(user, success, error) {
+			// var deferred = $q.defer();
+			console.log(JSON.stringify(user));
+			$http.post("/Angular_Jersy_Mongo_Tomcat_App/rest/qascript/login",
+					user).success(function(result) {
+						console.log(result.data+" :result ");
+				userInfo = {
+					//accessToken : result.data.access_token,
+					//email : result.data.email
+				};
+				success();
+				$window.sessionStorage["userInfo"] = JSON.stringify(userInfo);
+				// deferred.resolve(userInfo);
+			}).error(error);
+
+			// return deferred.promise;
+		}
+	}
+
+
+	factory.getUserInfo = function() {
+		return userInfo;
+	}
+
 	return factory;
-	/*return $resource('/ngdemo/rest/qascript', {});*/
-	
-	/*return $resource('/ngdemo/rest/qascript' , {} , {
-		post : {
-					method : 'POST'
-				}	
-		});*/
 });
