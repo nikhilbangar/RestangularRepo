c220e46f401d6d58f0763db4dc7fdb51268d3b5c
Sanjay Prusti <sanjay13prusti@gmail.com>
2014-10-29 15:22:56 +0530

    initial commit

--- Angular_Jersy_Mongo_Tomcat_App/src/main/java/org/atmecs/rest/RestService.java
+++ Angular_Jersy_Mongo_Tomcat_App/src/main/java/org/atmecs/rest/RestService.java
@@ -1,5 +1,66 @@
 package org.atmecs.rest;
 
-public class RestService {
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.Map;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.atmecs.service.SpreadService;
+
+import com.mongodb.BasicDBObject;
+
+/***
+ * 
+ * @author Nikhil
+ * @date 10-oct-2014
+ * @description : this class defines rest full api
+ */
+
+@Path("/qascript")
+public class RestService {
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public LinkedList<Map<String, Object>> getSpreadDataInJSON()
+			throws UnknownHostException {
+		SpreadService spreadService = new SpreadService();
+		System.out.println("sbafs");
+		return spreadService.getSpreadData();
+	}
+
+	@POST
+	@Path("/post")
+	// @Consumes({"application/json,text/plain,text/html"})
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void postSpreadSheetInJSON(BasicDBObject listOfObjects)
+			throws UnknownHostException {
+		System.out.println("list :" + listOfObjects);
+		// System.out.println("list :" + listOfObjects);
+		SpreadService service = new SpreadService();
+		BasicDBObject basicDBObject = new BasicDBObject();
+		basicDBObject.put("email", "fsdf@dfsdf");
+		basicDBObject.put("username", "djkfsdf");
+		basicDBObject.put("password", "paee");
+		service.postUserData(basicDBObject);
+		// service.postSpreadSheetData(listOfObjects);
+		// System.out.println("inside post of QAScriptRestService : "+qaScript);
+		// return Response.status(201).entity("sfs").build();
+	}
+
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String login(BasicDBObject dbObject) {
+		System.out.println("login has executed: "+dbObject);
+		return "user";
+
+	}
 }
--- Angular_Jersy_Mongo_Tomcat_App/src/main/java/org/atmecs/util/DBUtil.java
+++ Angular_Jersy_Mongo_Tomcat_App/src/main/java/org/atmecs/util/DBUtil.java
@@ -1,5 +1,38 @@
 package org.atmecs.util;
 
-public class DBUtil {
+import java.net.UnknownHostException;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+/***
+ * 
+ * @author Nikhil
+ * @date 10-oct-2014
+ * @description : this class defines utility methods and properties to
+ *              communicate with mongodb
+ */
+public class DBUtil {
+	private MongoClient mongoClient;
+	private DB db;
+	private DBCollection table;
+	private static DBUtil dbUtil;
+
+	private DBUtil() throws UnknownHostException {
+		mongoClient = new MongoClient();
+		db = mongoClient.getDB("qa");
+		table = db.getCollection("script");
+	}
+
+	public static DBUtil createInstance() throws UnknownHostException {
+		if (dbUtil == null) {
+			return new DBUtil();
+		}
+		return null;
+	}
+
+	public DBCollection getTable() {
+		return table;
+	}
 }
